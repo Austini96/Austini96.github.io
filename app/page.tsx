8c53fd2a80ebd3da5e9b16ec663c3be97d7db2b5
Austin Lee <austin-lee@uiowa.edu>
2025-06-03 19:40:07 -0500

    Original Version

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function Home() {
-  // Redirect to the static HTML file in the public directory
-  redirect("/index.html")
-
-  // This won't be reached due to the redirect
-  return null
-}
